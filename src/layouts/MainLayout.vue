f41e710602b2ea9e0285e18e2d91b6df5891465a
zjhgx163 <zjhgx163@163.com>
2022-05-31 18:34:02 +0800

    领券颜色改成红色

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -18,7 +18,7 @@
             class="col-2 lt-sm col-sm-auto text-overline text-italic items-center self-center text-bold YL__coupon_text_effect"
             v-bind:class="{ hidden: isCouponHidden }"
           >
-            <router-link :to="{ path: '/coupon/0' }"> 领券 </router-link>
+            <router-link :to="{ path: '/coupon/0' }" class="text-accent"> 领券 </router-link>
           </div>
           <div
             class="col-12 col-sm-6 q-px-xs q-pt-xs column justify-center"
