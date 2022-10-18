372ea26254c4c7aa59ba1edb1d12a806347ad64d
zjhgx163 <zjhgx163@163.com>
2022-10-19 01:02:22 +0800

    <q-img>长按无法保存图片，改成<img>

--- src/pages/YunpanItemDetailShare.vue
+++ src/pages/YunpanItemDetailShare.vue
@@ -63,11 +63,10 @@
               </q-item-section>
             </q-item>
             <q-item class="flex-center">
-              <q-img
-                width="50%"
+              <img
+                style="width: 70%; height: 100%"
                 src="https://private-david.oss-cn-beijing.aliyuncs.com/qrcode_for_laohuweinifuwu.jpeg"
-              >
-              </q-img>
+              />
             </q-item>
             <div class="q-pt-sm" ref="scrollYunpanDetailtRef">
               <!--   当使用:scroll-target 时，被指定的container必须要有 style="overflow: auto; max-height: 3000px" -->
