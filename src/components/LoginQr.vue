4e7c41a10824f85ec587b767250e6f5aae17a006
zjhgx163 <zjhgx163@163.com>
2022-10-26 23:40:37 +0800

    富文本initial

--- src/components/LoginQr.vue
+++ src/components/LoginQr.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-dialog v-model="loginCard" @hide="dialogHide">
+  <q-dialog v-model="isLoginCard" @hide="dialogHide">
     <q-card class="YL__login_card">
       <q-card-section class="column flex-center q-pt-lg q-pb-sm">
         <div class="text-subtitle1">微信扫码登陆</div>
@@ -8,7 +8,7 @@
         <div class="column flex-center" style="width: 150px; height: 150px">
           <q-spinner-facebook color="accent" size="2em" v-bind:class="{ hidden: !isLoadingQr }" />
           <q-img
-            :src="wechatQr"
+            :src="wechatQrSrc"
             position="50% 50%"
             class="flex-center"
             v-bind:class="{ hidden: isLoadingQr }"
@@ -39,6 +39,33 @@ export default {
   data() {
     return {};
   },
+  computed: {
+    loadingQr: {
+      set: function (newV) {
+        // this.loginCard = newV;
+      },
+      get: function () {
+        return this.isLoadingQr;
+      },
+    },
+    isLoginCard: {
+      set: function (newV) {
+        this.$emit('login-card-changed', newV);
+        // this.loginCard = newV;
+      },
+      get: function () {
+        return this.loginCard;
+      },
+    },
+    wechatQrSrc: {
+      set: function (newV) {
+        // this.loginCard = newV;
+      },
+      get: function () {
+        return this.wechatQr;
+      },
+    },
+  },
   props: ['isLoadingQr', 'loginCard', 'wechatQr'], // 微信auth code
 
   mounted() {
@@ -47,8 +74,10 @@ export default {
 
   methods: {
     dialogHide() {
+      console.log('dialogHide');
       clearInterval(this.timer);
-      this.isLoadingQr = false;
+      this.loadingQr = false;
+      // this.loginCard = false;
     },
   },
 };
---
+++ src/components/WysiwygEditor.vue
@@ -0,0 +1,190 @@
+<template>
+  <q-dialog v-model="editorShow" :maximized="maxized" @hide="dialogHide">
+    <q-card class="YL__editor_card">
+      <q-bar>
+        <!-- <q-icon name="network_wifi" />
+        <q-icon name="network_cell" />
+        <q-icon name="battery_full" /> -->
+        <!-- <div>9:34</div> -->
+        <q-space />
+        <q-btn dense flat icon="close" v-close-popup>
+          <q-tooltip>Close</q-tooltip>
+        </q-btn>
+      </q-bar>
+      <q-card-section>
+        <q-form @submit="onSubmit" class="q-gutter-sm">
+          <div class="q-gutter-sm">
+            <q-input
+              dense
+              outlined
+              standout="bg-yellow"
+              v-model="title"
+              placeholder="标题"
+              hide-bottom-space
+              type="text"
+            />
+          </div>
+          <q-editor
+            ref="editor"
+            v-model="post"
+            height="400px"
+            :definitions="{
+              upload: {
+                tip: 'Upload to cloud',
+                icon: 'photo',
+                handler: uploadIt,
+              },
+            }"
+            :toolbar="[
+              ['bold', 'italic', 'strike', 'underline'],
+              ['link'],
+              ['upload'],
+              ['undo', 'redo'],
+            ]"
+          />
+          <q-select
+            outlined
+            rounded
+            standout="bg-yellow"
+            v-model="tag"
+            :options="options"
+            label="标签"
+            dense
+          />
+
+          <div class="q-mt-sm">
+            <q-btn label="提交" type="submit" color="light-green" size="xs" />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+<style lang="sass">
+.YL
+  &__editor_card
+    @media(min-width: $breakpoint-xs-max)
+      width: 600px
+    @media(max-width: $breakpoint-xs-max)
+      width: 100%
+</style>
+<script>
+import { Screen } from 'quasar';
+
+export default {
+  name: 'WysisygEditor',
+  data() {
+    return {
+      post: '',
+      title: '',
+      options: ['影视', '动漫', '学习', '游戏/软件', '音乐/音频', '图片', '书籍', '求资源', '其他'],
+      tag: '',
+      maxized: Screen.gt.sm ? false : true,
+    };
+  },
+  computed: {
+    editorShow: {
+      set: function (newV) {
+        this.$emit('editor-show-changed', newV);
+        // this.isEditorShow = newV;
+      },
+      get: function () {
+        return this.isEditorShow;
+      },
+    },
+  },
+  props: ['isEditorShow'], // 微信auth code
+
+  mounted() {
+    console.log('WysisygEditor is mounted');
+  },
+
+  methods: {
+    dialogHide() {},
+    uploadIt() {
+      this.$refs.editor.focus();
+      //   const post = this.post;
+      // create an input file element to open file dialog
+      const input = document.createElement('input');
+      input.type = 'file';
+      input.accept = '.png, .jpg, .jpeg, .gif'; // file extensions allowed
+      let file;
+      input.onchange = (_) => {
+        const files = Array.from(input.files);
+        file = files[0];
+
+        // lets load the file as dataUrl
+        const reader = new FileReader();
+        let dataUrl = '';
+        reader.onloadend = function () {
+          dataUrl = reader.result;
+          // insert result to the cursor of your post
+          document.execCommand(
+            'insertHTML',
+            true,
+            '<div><img src="' + dataUrl + '" style="width:50%; height:50%" /></div>',
+          );
+
+          //   post.body += '<div><img src="' + dataUrl + '" style="width:50%; height:50%" /></div>';
+          //   console.log(post.body);
+        };
+        reader.readAsDataURL(file);
+      };
+      input.click();
+    },
+    onSubmit() {
+      if (this.title == '') {
+        this.$q.notify({
+          type: 'negative',
+          message: '请输入标题',
+          icon: 'warning',
+        });
+        return;
+      }
+      if (this.tag == '') {
+        this.$q.notify({
+          type: 'negative',
+          message: '请选择标签',
+          icon: 'warning',
+        });
+        return;
+      }
+      if (this.post == '') {
+        this.$q.notify({
+          type: 'negative',
+          message: '请输入内容',
+          icon: 'warning',
+        });
+        return;
+      }
+      this.$axios
+        .post(`${global.config.domain}/yunpan/post`, {
+          title: this.title,
+          tag: this.tag,
+          content: this.post,
+        })
+        .then((res) => {
+          if (res.data.code < 0) {
+            if (!this.$q.localStorage.has('userInfo')) {
+              if (this.isWeixin()) {
+                window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa249d330e183eb43&redirect_uri=https://www.hjdang.com/auth/${this.item.id}&response_type=code&scope=snsapi_userinfo&state=yunpanItem#wechat_redirect`;
+              } else {
+                this.$router.push({ path: 'login' });
+              }
+            } else {
+              this.$q.notify({
+                type: 'negative',
+                icon: 'warning',
+                message: `${res.data.msg}`,
+              });
+            }
+          } else {
+            this.content = '';
+            this.title = '';
+            this.$emit('editor-show-changed', false);
+          }
+        });
+    },
+  },
+};
+</script>
