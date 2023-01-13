15d0d510f2ad146739c5ffb3a12d5e2cafab7cee
zjhgx163 <zjhgx163@163.com>
2023-01-13 23:51:51 +0800

    优量汇 插件广告 测试

--- src/layouts/YunpanLayout.vue
+++ src/layouts/YunpanLayout.vue
@@ -326,7 +326,7 @@ export default {
   },
 };
 </script>
-<!--
+
 <script type="text/javascript">
 // H5 SDK 在线文档地址：http://developers.adnet.qq.com/doc/web/js_develop
 // 全局命名空间申明TencentGDT对象
@@ -367,4 +367,3 @@ window.TencentGDT.push({
   h && h.insertBefore(s, h.firstChild);
 })();
 </script>
--->
