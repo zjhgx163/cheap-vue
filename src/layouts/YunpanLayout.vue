db484cacdefc59cea52f080c1ad2584b22d3bb7f
zjhgx163 <zjhgx163@163.com>
2023-01-14 21:36:22 +0800

    信息流广告 测试

--- src/layouts/YunpanLayout.vue
+++ src/layouts/YunpanLayout.vue
@@ -233,7 +233,7 @@ export default {
       type: 'native', // {String} - 原生广告类型 - 必填
       // muid_type: '1', // {String} - 移动终端标识类型，1：imei，2：idfa，3：mac号 - 选填
       // muid: '******', // {String} - 加密终端标识，详细加密算法见API说明 -  选填
-      count: 1, // {Number} - 拉取广告的数量，默认是3，最高支持10 - 选填
+      count: 2, // {Number} - 拉取广告的数量，默认是3，最高支持10 - 选填
       onComplete: function (res) {
         if (res && res.constructor === Array) {
           // 原生模板广告位调用 window.TencentGDT.NATIVE.renderAd(res[0], 'containerId') 进行模板广告的渲染
