6c0a652d9063fd56ecdef276c45cdd10b5d7bae0
zjhgx163 <zjhgx163@163.com>
2023-04-03 00:10:53 +0800

    调整按钮大小

--- src/layouts/YunpanLayout.vue
+++ src/layouts/YunpanLayout.vue
@@ -111,30 +111,28 @@
       </router-view>
     </q-page-container>
 
-    <q-footer reveal class="bg-secondary q-pt-none q-pb-xs">
+    <q-footer class="bg-secondary q-pt-none q-pb-xs">
       <q-toolbar class="items-center justify-between q-px-lg q-py-xs" v-if="$route.meta.isList">
         <q-btn
           flat
-          size="0.7em"
+          size="0.8em"
           color="pink-4"
           label="今日好价"
           stack
           dense
           icon="o_shopping_cart"
           to="/"
-          fab-mini
         />
         <q-btn
           color="pink-4"
           rounded
-          size="0.7em"
+          size="0.8em"
           dense
-          fab-mini
           icon="o_add"
           @click="addYunpanItem"
           class="absolute-center"
         />
-        <q-btn flat size="0.7em" icon="shop" text-color="white" dense />
+        <q-btn flat size="0.8em" icon="shop" text-color="white" dense />
       </q-toolbar>
       <q-item v-if="!$route.meta.isList">
         <q-item-section top side class="text-bold"> 声明：</q-item-section>
